85ddf40d01dea88c3ab3b629ef6b9b7579783845
VIOLE-ASUS\viole <vgaitan@derivsoft.com>
2022-06-18 15:32:42 -0600

    actualizando properties

--- src/main/java/com/vio/microservices/limitsservice/configuration/Configuration.java
+++ src/main/java/com/vio/microservices/limitsservice/configuration/Configuration.java
@@ -1,7 +1,9 @@
 package com.vio.microservices.limitsservice.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+@Component
 @ConfigurationProperties("limits-service")
 public class Configuration {
 	private int minimum;
