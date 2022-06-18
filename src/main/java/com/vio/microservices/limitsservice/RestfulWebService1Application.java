6a7b3b150506237d54e06e85303025a853a90ec1
VIOLE-ASUS\viole <vgaitan@derivsoft.com>
2022-06-17 23:03:55 -0600

    carga inicial

---
+++ src/main/java/com/vio/microservices/limitsservice/RestfulWebService1Application.java
@@ -0,0 +1,13 @@
+package com.vio.microservices.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebService1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vio/microservices/limitsservice/RestfulWebService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vio.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
